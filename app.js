632b4f06d55386063bd3f1af9c8af565a98d37b0
unknown <JNKarif>
2024-08-28 13:23:30 +0600

    dummy-api

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const api_url = "https://api.quotable.io/random";
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const api_url = "https://dummyjson.com/quotes";
 
-async function getquote(url) {
+async function getQuote(url) {
   const response = await fetch(url);
   let data = await response.json();
-  console.log(data);
+  console.log(data.quotes[3].quote);
+  quote.innerHTML = data.quotes[3].quote;
+  author.innerHTML = data.quotes[3].author;
 }
-getquote(api_url);
+
+getQuote(api_url);
